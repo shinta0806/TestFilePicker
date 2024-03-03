969e9db834019bc9264e8861a302393eb477678b
shinta0806 <shinta.0806@gmail.com>
2024-03-03 09:08:42 +0900

    Ctrl+S で FilePicker がファイル名入力を受け付けない。

--- TestFilePicker/Views/MainPage.xaml.cs
+++ TestFilePicker/Views/MainPage.xaml.cs
@@ -1,19 +1,45 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml.Controls;
 
 using TestFilePicker.ViewModels;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using System.Diagnostics;
 
 namespace TestFilePicker.Views;
 
 public sealed partial class MainPage : Page
 {
-    public MainViewModel ViewModel
-    {
-        get;
-    }
+	public MainPage()
+	{
+		ViewModel = App.GetService<MainViewModel>();
+		InitializeComponent();
+		MenuFlyoutItemSaveClickedCommand = new RelayCommand(MenuFlyoutItemSaveClicked);
+	}
 
-    public MainPage()
-    {
-        ViewModel = App.GetService<MainViewModel>();
-        InitializeComponent();
-    }
+	public MainViewModel ViewModel
+	{
+		get;
+	}
+
+
+	#region Save
+	public RelayCommand MenuFlyoutItemSaveClickedCommand
+	{
+		get;
+	}
+
+	public async void MenuFlyoutItemSaveClicked()
+	{
+		FileSavePicker fileSavePicker = App.MainWindow.CreateSaveFilePicker();
+		fileSavePicker.FileTypeChoices.Add("Text file", [".txt"]);
+		StorageFile? file = await fileSavePicker.PickSaveFileAsync();
+		if (file == null)
+		{
+			return;
+		}
+		Debug.WriteLine(file.Path);
+	}
+	#endregion
+
 }
