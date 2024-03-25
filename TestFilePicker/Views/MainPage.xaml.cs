7ff7f0261f1f5cca8e001a16ab7d770dc6ccd463
shinta0806 <shinta.0806@gmail.com>
2024-03-25 15:58:42 +0900

    WindowsAppSDK チケット #2504 についてコメント記載。

--- TestFilePicker/Views/MainPage.xaml.cs
+++ TestFilePicker/Views/MainPage.xaml.cs
@@ -1,10 +1,8 @@
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.UI.Xaml.Controls;
-
 using TestFilePicker.ViewModels;
-using Windows.Storage.Pickers;
 using Windows.Storage;
-using System.Diagnostics;
+using Windows.Storage.Pickers;
 
 namespace TestFilePicker.Views;
 
@@ -44,9 +42,27 @@ public sealed partial class MainPage : Page
 		}
 		catch (Exception ex)
 		{
+			// 管理者として実行すると例外が発生する（別の問題）
+			// WindowsAppSDK チケット #2504 Trying to use a FileOpenPicker while running the app as Administrator will crash the app
+			// https://github.com/microsoft/WindowsAppSDK/issues/2504
 			ViewModel.Notice = ex.Message + "\n" + ex.StackTrace;
+
+			// 【例外発生整理】
+			// ・ログインユーザーにかかわらず、「管理者として実行」すると例外
+			//   → 一般ユーザーでそのまま実行：OK
+			//   → 一般ユーザーで管理者として実行：NG
+			//   → 管理者ユーザーでそのまま実行：OK
+			//   → 管理者ユーザーで管理者として実行：NG
+
+			// 【ビルド整理】
+			// ・異なるユーザーでビルド・実行しようとすると DEP0700 エラー「～競合するパッケージは XXXX です」となる
+			// ・管理者権限で PowerShell を起動し、
+			//   Get-AppxPackage -AllUsers | Select-String "XXXX"
+			//   → PackageFullName "XXXX-yyyy" が表示される
+			//   Remove-AppPackage -Package "XXXX-yyyy" -AllUsers
+			//   → パッケージがアンインストールされるので、異なるユーザーでビルド・実行できるようになる
+			//   → 一般ユーザーでログインして管理者として Visual Studio を実行すると別のエラー（パッケージの再インストールはブロックされました）になるので、管理者は管理者としてログインする
 		}
 	}
 	#endregion
-
 }
